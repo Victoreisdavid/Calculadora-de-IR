4742443487c884147c30c634a2a8d26915761bbe
Victoreisdavid <victorreis5919@gmail.com>
2022-08-30 02:25:50 -0300

    navbar link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,10 @@
 import style from "../styles/navbar.module.scss";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <nav id={style.navbar}>
-            <a href="/">Como calcular</a>
+            <Link href="https://imposto-de-renda.vercel.app/"><a target="_blank">Como calcular</a></Link>
         </nav>
     )
 }
